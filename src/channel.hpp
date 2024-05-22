0a05855c74797bd0511286c452884a908dfb1444
Ivgorbunov <ivgorbunov@me.com>
2024-05-22 22:05:19 +0300

    better

--- src/channel.hpp
+++ src/channel.hpp
@@ -5,7 +5,15 @@ bool try_connect(IProver* prover) {
     Verificator* verificator = Verificator::GetInstance();
     verificator->Init(prover->Init());
     Message now{0, {}, Respond::kContinue};
+    int cnt = 0;
     while (now.resp == Respond::kContinue) {
+        if (cnt % 2 == 0) {
+            std::cout << "-------------------------\n";
+            std::cout << "Iteration: " << cnt / 2 << std::endl;
+            std::cout << "-------------------------\n";
+
+        }
+        cnt++;
         auto was = now.iter;
         now = prover->Respond(now);
         if (now.iter != was || now.resp != Respond::kProver) { // In case he is trying to fool us
